d5bd3adbe88bff279a9db018462078efd5b11791
MSturges <Sturges3232@gmail.com>
2016-08-03 11:04:20 -0600

    idk if this works

--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -13,7 +13,7 @@
 
   function MapController ($log, $scope, NgMap, $rootScope) {
     $scope.showMap = false;
-    
+
     $scope.checkRoute = function() {
       if ($rootScope.currentNavItem === 'home') {
         $scope.showMap = true;
@@ -22,6 +22,17 @@
       }
       return $scope.showMap
     }
+
+    $scope.markers = [
+      {pos: [40.0150, -105.2705], infoHead: 'i\m a header1', infoContent: 'I\m content1', id:1},
+      {pos: [40.0260, -105.2805], infoHead: 'i\m a header2', infoContent: 'I\m content2', id:2},
+      {pos: [40.0370, -105.2905], infoHead: 'i\m a header3', infoContent: 'I\m content3', id:3},
+      {pos: [40.0483, -105.3005], infoHead: 'i\m a header4', infoContent: 'I\m content4', id:4},
+      {pos: [40.0594, -105.3105], infoHead: 'i\m a header5', infoContent: 'I\m content5', id:5}
+    ]
+
+    $scope.infoWindow
+
 
 
     //  $scope.currentNavItem = 'home';
