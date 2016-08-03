34e41cf024ca000f4b0a9d6951a1a10a3543429f
MSturges <Sturges3232@gmail.com>
2016-08-03 13:32:41 -0600

    pre leaflet

--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -23,21 +23,27 @@
       return $scope.showMap
     }
 
-    $scope.markers = [
-      {pos: [40.0150, -105.2705], infoHead: 'i\m a header1', infoContent: 'I\m content1', id:1},
-      {pos: [40.0260, -105.2805], infoHead: 'i\m a header2', infoContent: 'I\m content2', id:2},
-      {pos: [40.0370, -105.2905], infoHead: 'i\m a header3', infoContent: 'I\m content3', id:3},
-      {pos: [40.0483, -105.3005], infoHead: 'i\m a header4', infoContent: 'I\m content4', id:4},
-      {pos: [40.0594, -105.3105], infoHead: 'i\m a header5', infoContent: 'I\m content5', id:5}
-    ]
-
-    $scope.infoWindow
-
-
-
-    //  $scope.currentNavItem = 'home';
-    NgMap.getMap().then(function(map) {
-    });
+    // $scope.markers = [
+    //   {pos: [40.0150, -105.2705], id:1},
+    //   {pos: [40.0260, -105.2805], id:2},
+    //   {pos: [40.0370, -105.2905], id:3},
+    //   {pos: [40.0483, -105.3005], id:4},
+    //   {pos: [40.0594, -105.3105], id:5}
+    // ]
+    //
+    // $scope.infoWindow = [
+    //   {infoHead: 'i\m a header1', infoContent: 'I\m content1', id:1},
+    //   {infoHead: 'i\m a header2', infoContent: 'I\m content2', id:2},
+    //   {infoHead: 'i\m a header3', infoContent: 'I\m content3', id:3},
+    //   {infoHead: 'i\m a header4', infoContent: 'I\m content4', id:4},
+    //   {infoHead: 'i\m a header5', infoContent: 'I\m content5', id:5},
+    // ]
+    //
+    //
+    //
+    // //  $scope.currentNavItem = 'home';
+    // NgMap.getMap().then(function(map) {
+    // });
 
   }
 
